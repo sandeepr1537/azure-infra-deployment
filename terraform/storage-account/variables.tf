2a3d3bf8e2b85f4392a17fda227986a1785f7f92
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-08-29 12:45:02 +1200

    Create variables.tf

---
+++ terraform/storage-account/variables.tf
@@ -0,0 +1,14 @@
+variable "storage_account_name" {
+  description = "The name of the storage account."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The location where resources will be created."
+  type        = string
+}
