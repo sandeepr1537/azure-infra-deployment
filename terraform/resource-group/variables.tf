912336e46526c05e81c840334ca8591b922d9bdb
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-08-29 12:43:17 +1200

    Update variables.tf

--- terraform/resource-group/variables.tf
+++ terraform/resource-group/variables.tf
@@ -1,1 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
 
+variable "location" {
+  description = "The location where resources will be created."
+  type        = string
+}
